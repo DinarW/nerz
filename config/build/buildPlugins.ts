d27675f97feefdcc9efa8715317176718ddbd9be
DinarW <dinarmkbg@gmail.com>
2023-01-29 16:07:43 +0300

    feat: config decomposition

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,13 @@
+import { ProgressPlugin, WebpackPluginInstance } from "webpack";
+import HTMLWebpackPlugin from "html-webpack-plugin";
+import {BuildOptions} from "./types/config";
+
+export function buildPlugins({ paths }: BuildOptions): WebpackPluginInstance[] {
+
+    return [
+        new HTMLWebpackPlugin({
+            template: paths.html,
+        }),
+        new ProgressPlugin(),
+    ];
+}
