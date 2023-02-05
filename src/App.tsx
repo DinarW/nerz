d6b237519b58a719a3cf3a7c7bf4a1e3f789c84c
DinarW <dinarmkbg@gmail.com>
2023-02-05 19:48:57 +0300

    feat: add theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './styles/index.scss';
+import { useTheme } from "./theme/useTheme";
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
       <h1>Nerz</h1>
+      <button onClick={toggleTheme}>Toggle theme</button>
     </div>
   )
 }
