ef1b25bc2d77e819a105bab1dfced7ad7d57a5c3
DinarW <dinarmkbg@gmail.com>
2023-02-10 22:24:06 +0300

    refactoring: feature sliced design

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,23 +1,25 @@
-import { RuleSetRule } from "webpack";
+import webpack from "webpack";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import { BuildOptions } from "./types/config";
+import {BuildOptions} from "./types/config";
 
-export function buildLoaders({ isDev }: BuildOptions): RuleSetRule[] {
+export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
             isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
             {
-                loader: 'css-loader',
+                loader: "css-loader",
                 options: {
                     modules: {
                         auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-                        localIdentName: isDev ? '[path][name]__[local]' : '[hash:base64:8]',
+                        localIdentName: isDev
+                            ? '[path][name]__[local]--[hash:base64:5]'
+                            : '[hash:base64:8]'
                     },
-                },
+                }
             },
-            'sass-loader',
+            "sass-loader",
         ],
     }
 
@@ -25,10 +27,10 @@ export function buildLoaders({ isDev }: BuildOptions): RuleSetRule[] {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
-    };
+    }
 
     return [
         typescriptLoader,
         cssLoader,
-    ];
+    ]
 }
