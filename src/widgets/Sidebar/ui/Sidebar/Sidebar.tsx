63962a23b4c57785fd4b1bf4122cea1ec54ac7df
DinarW <dinarmkbg@gmail.com>
2023-02-12 18:45:18 +0300

    chore: fix lint errors

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -17,8 +17,15 @@ export const Sidebar = ({ className }: SidebarProps) => {
 
     return (
         <div
-            className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
+            className={
+                classNames(
+                    cls.Sidebar,
+                    { [cls.collapsed]: collapsed },
+                    [className],
+                )
+            }
         >
+            {/* eslint-disable-next-line i18next/no-literal-string */}
             <button type="button" onClick={onToggle}>toggle</button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
