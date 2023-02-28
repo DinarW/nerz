d070b6b17ffe332a7238f8f2cef01f97ae5d8efe
DinarW <dinarmkbg@gmail.com>
2023-03-01 00:00:12 +0300

    feature: login and logout

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
 import { LoginForm } from './LoginForm';
 
 export default {
@@ -14,3 +17,37 @@ const Template: ComponentStory<typeof LoginForm> = (args) => <LoginForm {...args
 
 export const Primary = Template.bind({});
 Primary.args = {};
+Primary.decorators = [StoreDecorator({
+    loginForm: {
+        username: '123',
+        password: '123',
+    },
+})];
+
+export const PrimaryDark = Template.bind({});
+PrimaryDark.args = {};
+PrimaryDark.decorators = [
+    StoreDecorator({
+        loginForm: {
+            username: '123',
+            password: '123',
+        },
+    }),
+    ThemeDecorator(Theme.DARK),
+];
+
+export const withError = Template.bind({});
+withError.args = {};
+withError.decorators = [StoreDecorator({
+    loginForm: {
+        username: '123',
+        password: '123',
+        error: 'ERROR',
+    },
+})];
+
+export const Loading = Template.bind({});
+Loading.args = {};
+Loading.decorators = [StoreDecorator({
+    loginForm: { isLoading: true },
+})];
--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -1,29 +1,64 @@
-import React from 'react';
+import React, { memo, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
-import { Button } from 'shared/ui/Button/Button';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { Text, TextTheme } from 'shared/ui/Text/Text';
 import { Input } from 'shared/ui/Input/Input';
+import { getLoginState } from '../../model/selectors/getLoginState/getLoginState';
+import { loginByUsername } from '../../model/services/loginByUsername/loginByUsername';
+import { loginActions } from '../../model/slice/loginSlice';
 import cls from './LoginForm.module.scss';
 
 interface LoginFormProps {
   className?: string;
 }
 
-export const LoginForm = ({ className }: LoginFormProps) => {
+export const LoginForm = memo(({ className }: LoginFormProps) => {
     const { t } = useTranslation();
+    const dispatch = useDispatch();
+    const {
+        username, password, error, isLoading,
+    } = useSelector(getLoginState);
+
+    const onChangeUsername = useCallback((value: string) => {
+        dispatch(loginActions.setUserName(value));
+    }, [dispatch]);
+
+    const onChangePassword = useCallback((value: string) => {
+        dispatch(loginActions.setPassword(value));
+    }, [dispatch]);
+
+    const onLoginClick = useCallback(() => {
+        dispatch(loginByUsername({ username, password }));
+    }, [dispatch, password, username]);
 
     return (
         <div className={classNames(cls.LoginForm, {}, [className])}>
+            <Text title={t('Авторизация')} />
+            {error && <Text text={t('Вы ввели неверный логин или пароль')} theme={TextTheme.ERROR} />}
             <Input
                 className={cls.input}
                 placeholder={t('Введите логин')}
+                onChange={onChangeUsername}
+                value={username}
                 autofocus
             />
             <Input
+                type="password"
                 className={cls.input}
                 placeholder={t('Введите пароль')}
+                onChange={onChangePassword}
+                value={password}
             />
-            <Button className={cls.loginBtn}>{t('Войти')}</Button>
+            <Button
+                className={cls.loginBtn}
+                theme={ButtonTheme.OUTLINE}
+                onClick={onLoginClick}
+                disabled={isLoading}
+            >
+                {t('Войти')}
+            </Button>
         </div>
     );
-};
+});
